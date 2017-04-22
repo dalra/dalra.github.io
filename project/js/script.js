50f03b2885a8ce50dba6df367ac8d063a8d1a394
MSDN-SPECIAL\Administrator <ljhye212@naver.com>
2017-04-22 09:38:18 +0900

    Header hover작업

--- project/js/script.js
+++ project/js/script.js
@@ -2,4 +2,15 @@ var doc = document;
 
 $(doc).ready(function(){
     
+    $('.gslider').slick({
+        arrows : false
+    });
+    
+    $('.next').on('click', function(){
+        $('.gslider').slick('slickNext');
+    });
+    
+    $('.prev').on('click', function(){
+        $('.gslider').slick('slickPrev');
+    });
 });
